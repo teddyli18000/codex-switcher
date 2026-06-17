32c7912a98201ea5c87640b66480b30ab74c351e
Xinchen Lee <teddyli18000@gmail.com>
2026-06-17 21:38:40 +0800

    Keep compatibility export without update logic

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { AccountCard } from "./AccountCard";
 export { UsageBar } from "./UsageBar";
 export { AddAccountModal } from "./AddAccountModal";
+
+export function UpdateChecker() {
+  return null;
+}
