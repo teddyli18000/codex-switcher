09429d79c903dc6b8baee384c6f4ba5c7f54271c
Xinchen Lee <teddyli18000@gmail.com>
2026-06-17 21:29:56 +0800

    Remove updater component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { AccountCard } from "./AccountCard";
 export { UsageBar } from "./UsageBar";
 export { AddAccountModal } from "./AddAccountModal";
-export { UpdateChecker } from "./UpdateChecker";
